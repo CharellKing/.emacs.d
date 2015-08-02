441eedddb4a788b78549d0c58f233b4101fcab3f
CharellKing <CharellkingQu@163.com>
2015-08-02 21:37:19 +0800

    加入括号自动完成

---
+++ lisp/init-autopair.el
@@ -0,0 +1,46 @@
+(require 'autopair)
+
+(autopair-global-mode 1)
+
+;; (defvar autopair-modes '(r-mode ruby-mode))
+;; (defun turn-on-autopair-mode () (autopair-mode 1))
+;; (dolist (mode autopair-modes) (add-hook (intern (concat (symbol-name mode) "-hook")) 'turn-on-autopair-mode))
+
+;; (require 'paredit)
+;; (defadvice paredit-mode (around disable-autopairs-around (arg))
+;;   "Disable autopairs mode if paredit-mode is turned on"
+;;   ad-do-it
+;;   (if (null ad-return-value)
+;;       (autopair-mode 1)
+;;     (autopair-mode 0)
+;;     ))
+
+;; (ad-activate 'paredit-mode)
+
+
+(add-hook 'c++-mode-hook
+	  #'(lambda ()
+	      (push ?{
+		    (getf autopair-dont-pair :comment))))
+
+(add-hook 'python-mode-hook
+	  #'(lambda ()
+	      (setq autopair-handle-action-fns
+		    (list #'autopair-default-handle-action
+			  #'autopair-python-triple-quote-action))))
+
+
+(add-hook 'c++-mode-hook
+	  #'(lambda ()
+	      (push '(?< . ?>)
+		    (getf autopair-extra-pairs :code))))
+
+(add-hook 'emacs-lisp-mode-hook
+	  #'(lambda ()
+	      (push '(?` . ?')
+		    (getf autopair-extra-pairs :comment))
+	      (push '(?` . ?')
+		    (getf autopair-extra-pairs :string))))
+
+(provide 'init-autopair)
+
