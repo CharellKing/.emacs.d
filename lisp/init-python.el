2971fa8cc51083c3320e97895ac2edbc838d67a4
CharellKing <CharellkingQu@163.com>
2015-08-03 16:34:06 +0800

    flymake启动失败时，禁止提示

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -5,4 +5,9 @@
 (define-key yas-minor-mode-map (kbd "C-c k") 'yas-expand)
 (define-key global-map (kbd "C-c o") 'iedit-mode)
 
+
+(defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
+  (setq flymake-check-was-interrupted t))
+(ad-activate 'flymake-post-syntax-check)
+
 (provide 'init-python)
