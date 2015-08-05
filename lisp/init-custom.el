66a7c97b44c84d7fc4bfd6a785fa1a91edb84dd1
CharellKing <CharellkingQu@163.com>
2015-08-05 18:45:36 +0800

    在mode line上显示字符集

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -21,6 +21,7 @@
 
 
 ;;字符集
+(set-language-environment "utf-8")
 (set-language-environment 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-clipboard-coding-system 'euc-cn)
@@ -33,9 +34,25 @@
 (setq-default pathname-coding-system 'utf-8)
 
 
-;;highlight-indent
+;;缩进高亮
 (require 'highlight-indentation)
 (set-face-background 'highlight-indentation-face "#e3e3d3")
 (set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
+
+;; 在mode line上显示文件字符集
+(defvar my-mode-line-coding-format
+  '(:eval
+    (let* ((code (symbol-name buffer-file-coding-system))
+	   (eol-type (coding-system-eol-type buffer-file-coding-system))
+	   (eol (if (eq 0 eol-type) "UNIX"
+		  (if (eq 1 eol-type) "DOS"
+		    (if (eq 2 eol-type) "MAC"
+		      "???")))))
+      (concat code " " eol " "))))
+(put 'my-mode-line-coding-format 'risky-local-variable t)
+(setq-default mode-line-format (substitute
+                                'my-mode-line-coding-format
+                                'mode-line-mule-info
+                                mode-line-format))
 
 (provide 'init-custom)
