a0aa19dcac28fcdf9bd6a793d7fe54b1d0ec14a2
CharellKing <CharellkingQu@163.com>
2015-08-05 15:29:20 +0800

    设置indentation的颜色

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -35,6 +35,7 @@
 
 ;;highlight-indent
 (require 'highlight-indentation)
-
+(set-face-background 'highlight-indentation-face "#e3e3d3")
+(set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
 
 (provide 'init-custom)
