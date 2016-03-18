9653a78c119cbe7e1d443d243e5cb79193b193a4
CharellKing <CharellkingQu@163.com>
2016-03-19 00:12:25 +0800

    增加linux下的剪贴板支持

--- install/kali_install.sh
+++ install/kali_install.sh
@@ -9,10 +9,10 @@ if [ "x${SYSTEM_TYPE}" != "xkali" ]; then
     exit 1
 fi
 
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.`date +%s`
-sudo cp ${CUR_DIR}/kali_conf/sources.list /etc/apt/sources.list
+# sudo cp /etc/apt/sources.list /etc/apt/sources.list.`date +%s`
+# sudo cp ${CUR_DIR}/kali_conf/sources.list /etc/apt/sources.list
 ${APT_GET} update
 
 ${APT_GET} install boot-repair
 
-${APT_GET} install network-manager-openvpn-gnome network-manager-pptp network-manager-pptp-gnome network-manager-strongswan network-manager-vpnc network-manager-vpnc-gnome
+${APT_GET} install network-manager-openvpn-gnome network-manager-pptp network-manager-pptp-gnome network-manager-strongswan network-manager-vpnc network-manager-vpnc-gnome xclip
