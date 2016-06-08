77ccbbcbec525216dd2e4af86e09ee10bf4ff511
CharellKing <CharellkingQu@163.com>
2016-06-08 11:46:07 +0800

    当hook python-mode时才加载python配置

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,13 +1,18 @@
-(elpy-enable)
+(defun my-python-mode-hook ()
+  "Hooks for Web mode."
+  (elpy-enable)
 
-(elpy-use-ipython)
+  (elpy-use-ipython)
 
-(define-key yas-minor-mode-map (kbd "C-c k") 'yas-expand)
-(define-key global-map (kbd "C-c o") 'iedit-mode)
+  (define-key yas-minor-mode-map (kbd "C-c k") 'yas-expand)
+  (define-key global-map (kbd "C-c o") 'iedit-mode)
 
+  (defun flymake-post-syntax-check (before flymake-force-check-was-interrupted)
+    (setq flymake-check-was-interrupted t))
+  (ad-activate 'flymake-post-syntax-check)
+)
 
-(defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
-  (setq flymake-check-was-interrupted t))
-(ad-activate 'flymake-post-syntax-check)
+(add-hook 'python-mode-hook  'my-python-mode-hook)
+
 
 (provide 'init-python)
