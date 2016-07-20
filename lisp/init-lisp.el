11e62e6a4820be4d48a99b89ec072e3cbb6dbc28
CharellKing <CharellkingQu@163.com>
2016-07-21 01:30:01 +0800

    修改lisp配置

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,24 +1,4 @@
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
 (require 'slime)
-;; (slime-setup
-;;  '(inferior-slime
-;;    slime-asdf
-;;    slime-autodoc
-;;    slime-banner
-;;    slime-c-p-c
-;;    slime-editing-commands
-;;    slime-fancy-inspector
-;;    slime-fancy
-;;    slime-fuzzy
-;;    slime-highlight-edits
-;;    slime-parse
-;;    slime-presentation-streams
-;;    slime-presentations
-;;    slime-references
-;;    slime-scratch
-;;    slime-tramp
-;;    slime-typeout-frame
-;;    slime-xref-browser))
-(slime-setup)
-
+(setq inferior-lisp-program "/usr/bin/sbcl")
+(slime-setup '(slime-company))
 (provide 'init-lisp)
